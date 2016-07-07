2b81d19bf1f514010a01290d3a41be5f53a3cf51
Jill Donohue <jillmd501@gmail.com>
2016-07-07 14:07:16 -0600

    Fix nav bar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-	root to: "welcome#index"
+  root to: "welcome#index"
+  get "/contact", to: "welcome#contact"
+  get "/inspiration", to: "welcome#inspiration"
+  get "/portfolio", to: "welcome#portfolio"
 end
